26f83db20da4cff9e7fba2eb305b724bc2c39daa
zaccaffey <caffeyzac@gmail.com>
2024-10-20 19:47:34 -0500

    Imprint Layout : Format layout for readability

--- keyboards/cyboard/imprint/imprint_number_row/keymaps/default/keymap.c
+++ keyboards/cyboard/imprint/imprint_number_row/keymaps/default/keymap.c
@@ -34,12 +34,12 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {  
 
     [0] = LAYOUT_num(
-        TD(TD_LOGOUT_EQL),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                                                                KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                                                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LCTL,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                                                                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                                                                                KC_N,    TD(TD_MUTE_M),    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                     KC_LGUI, KC_LALT, KC_ENT,  TD(TD_BACK_CHROME_DEL),  TD(TD_LOCK_ESC),                                                                    TD(TD_SCREENSHOT_SELECT), TD(TD_BACK_CHROME_BSPC), KC_SPC,  KC_LBRC, KC_RBRC,
-                                                       QK_CAPS_WORD_TOGGLE, TD(TD_BACK_VSCODE_HOME), TD(TD_BACK_END),                                       TD(TD_FORWARD_COPY),  TD(TD_FORWARD_VSCODE_PASTE),  MO(1)
+        TD(TD_LOGOUT_EQL),  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                                                  KC_6,                    KC_7,          KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_TAB,             KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                                                                  KC_Y,                    KC_U,          KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_LCTL,            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                                                                  KC_H,                    KC_J,          KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT,            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                                                                  KC_N,                    TD(TD_MUTE_M), KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                     KC_LGUI, KC_LALT, KC_ENT,  TD(TD_BACK_CHROME_DEL),  TD(TD_LOCK_ESC),                                    TD(TD_SCREENSHOT_SELECT), TD(TD_BACK_CHROME_BSPC), KC_SPC,        KC_LBRC, KC_RBRC,
+                                                       QK_CAPS_WORD_TOGGLE, TD(TD_BACK_VSCODE_HOME), TD(TD_BACK_END),            TD(TD_FORWARD_COPY), TD(TD_FORWARD_VSCODE_PASTE),  MO(1)
     ),
 
     [1] = LAYOUT_num(
